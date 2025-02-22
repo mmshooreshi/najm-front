d2cb3538f62dafc59d224b0aef1d7ded48a12fbd
mmshooreshi <mmshooreshi@gmail.com>
2025-02-22 16:04:44 +0330

    new sections added

---
+++ pages/buttons.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+      <h1>Enhanced Buttons</h1>
+      <ButtonComponent class="text-d4 text-demibold text-base hover:tracking-wide bg-[#014439] bg-opacity-90 text-white hover:bg-opacity-100 py-[20px] px-[90px]" label="با امکانات ما آشنا شو" @clicked="handleSubmit" />
+
+    </div>
+  </template>
+  
+  <script setup>
+  import ButtonComponent from "@/components/re/Btn.vue";
+  
+  const handleSubmit = () => {
+    console.log("Button clicked!");
+  };
+  </script>
+  
+  <style scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 15px;
+  }
+  </style>
+  
