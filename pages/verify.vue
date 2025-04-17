4c7875840672848ea7407658ecb90eae5d080c0c
mmshooreshi <mmshooreshi@gmail.com>
2025-04-17 19:09:35 +0330

    feature: login better

--- pages/verify.vue
+++ pages/verify.vue
@@ -3,7 +3,7 @@
     <AuthHeader icon="mdi:key" title="کد تایید رو وارد کن">
         <template #subtitle>
           <div class="text-xs md:text-sm">
-            به شماره شما {{ toPersianDigits(identifier) }} ارسال شد
+            به شماره شما {{ toPersianDigits(identifier) }} ارسال شد.
             <span @click="editNumber" class="text-blue-600 underline cursor-pointer">
               ویرایش
             </span>
@@ -47,14 +47,14 @@
   import BaseButton from '~/components/Base/BaseButton.vue'
   import { useAuth } from '~/composables/useAuth'
   import { toPersianDigits, toEnglishDigits } from '~/utils/digits'
-  
+
   const router = useRouter()
   const { identifier, token } = useAuth()
   const code = ref('')
   const otpLength = ref(6)
   
   // —— countdown timer logic ——
-  const initialSeconds = 94 // 1 minute 34 seconds
+  const initialSeconds = 10 // 1 minute 34 seconds
   const timer = ref<number>(initialSeconds)
   let timerInterval: number
   
@@ -82,10 +82,28 @@
     startTimer()
   }
   
+
+  async function pasteFromClipboard () {
+  try {
+    const raw = await navigator.clipboard.readText()
+    const digits = toEnglishDigits(raw).replace(/\D/g, '')
+    if (digits.length === otpLength.value) code.value = digits
+  } catch { /* user denied or API unsupported */ }
+}
+
+  function tryAutoPasteOnce (e: PointerEvent) {
+  window.removeEventListener('pointerdown', tryAutoPasteOnce)
+  pasteFromClipboard()
+}
   onMounted(() => {
     startTimer()
     window.addEventListener('focus', checkClipboard)
     clipboardInterval = window.setInterval(checkClipboard, 1000)
+
+    if ('clipboard' in navigator) {
+    window.addEventListener('pointerdown', tryAutoPasteOnce, { once: true })
+  }
+
   })
   
   onBeforeUnmount(() => {
@@ -111,20 +129,20 @@
   let lastText = ''
   
   async function checkClipboard() {
-    if (!document.hasFocus()) return
-    try {
-      const rawtext = await navigator.clipboard.readText()
-      const text = toEnglishDigits(rawtext).replace(/\D/g, '')
-      if (text && text !== lastText) {
-        const regex = new RegExp(`^\\d{${otpLength.value}}$`)
-        if (regex.test(text)) {
-          code.value = text
-        }
-        lastText = text
-      }
-    } catch {
-      // ignore
-    }
+    // if (!document.hasFocus()) return
+    // try {
+    //   const rawtext = await navigator.clipboard.readText()
+    //   const text = toEnglishDigits(rawtext).replace(/\D/g, '')
+    //   if (text && text !== lastText) {
+    //     const regex = new RegExp(`^\\d{${otpLength.value}}$`)
+    //     if (regex.test(text)) {
+    //       code.value = text
+    //     }
+    //     lastText = text
+    //   }
+    // } catch {
+    //   // ignore
+    // }
   }
   
   definePageMeta({ layout: 'auth' })
