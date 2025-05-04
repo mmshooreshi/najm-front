e024bb5805fb04c89e958348a03d792ac301d2f1
mmshooreshi <mmshooreshi@gmail.com>
2025-05-05 02:30:15 +0330

    final

--- components/Accordion.vue
+++ components/Accordion.vue
@@ -4,7 +4,7 @@
       <AccordionItem
       v-motion-pop-visible 
         v-for="(item, index) in items"
-        :delay="index*10"
+        :delay="index*50"
         :key="index"
         :title="item.title"
         :content="item.content"
