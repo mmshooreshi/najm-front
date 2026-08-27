b6ff73ce933eaf9010e515b4f1d0b27ed70f3fda
mmshooreshi <mmshooreshi@gmail.com>
2026-08-27 07:46:20 -0400

    fix(icons): implement comprehensive SVG vector paths for all dashboard navigation and action icons

--- components/admin/AdminIcon.vue
+++ components/admin/AdminIcon.vue
@@ -9,8 +9,71 @@ const props = withDefaults(defineProps<{
 </script>
 
 <template>
+  <!-- DASHBOARD / HOME / LAYOUT -->
   <svg
-    v-if="name === 'pencil'"
+    v-if="name === 'dashboard' || name === 'layout-dashboard' || name === 'gauge'"
+    :class="props.class"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M4 4h6v8h-6z" />
+    <path d="M4 16h6v4h-6z" />
+    <path d="M14 12h6v8h-6z" />
+    <path d="M14 4h6v4h-6z" />
+  </svg>
+
+  <svg
+    v-else-if="name === 'home'"
+    :class="props.class"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
+    <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
+    <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
+  </svg>
+
+  <svg
+    v-else-if="name === 'layout'"
+    :class="props.class"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
+    <path d="M4 9h16" />
+    <path d="M9 4v16" />
+  </svg>
+
+  <svg
+    v-else-if="name === 'menu'"
+    :class="props.class"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M4 6h16" />
+    <path d="M4 12h16" />
+    <path d="M4 18h16" />
+  </svg>
+
+  <!-- EDIT / PENCIL -->
+  <svg
+    v-else-if="name === 'pencil' || name === 'edit' || name === 'edit-3'"
     :class="props.class"
     viewBox="0 0 24 24"
     fill="none"
@@ -23,6 +86,86 @@ const props = withDefaults(defineProps<{
     <path d="M13.5 6.5l4 4" />
   </svg>
 
+  <!-- FILE / DOCUMENT / ARTICLE / BLOG -->
+  <svg
+    v-else-if="name === 'file' || name === 'file-text' || name === 'document' || name === 'article'"
+    :class="props.class"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+    <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
+    <path d="M9 13h6" />
+    <path d="M9 17h4" />
+  </svg>
+
+  <!-- MAIL / INBOX / REQUESTS -->
+  <svg
+    v-else-if="name === 'mail' || name === 'inbox' || name === 'envelope' || name === 'messages'"
+    :class="props.class"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" />
+    <path d="M3 7l9 6l9 -6" />
+  </svg>
+
+  <!-- CHAT / WHATSAPP / MESSAGING -->
+  <svg
+    v-else-if="name === 'chat' || name === 'message' || name === 'message-circle' || name === 'whatsapp'"
+    :class="props.class"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M3 20l1.3 -3.9a9 8 0 1 1 3.4 2.9l-4.7 1" />
+  </svg>
+
+  <!-- PHONE -->
+  <svg
+    v-else-if="name === 'phone' || name === 'call'"
+    :class="props.class"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+  </svg>
+
+  <!-- CALENDAR -->
+  <svg
+    v-else-if="name === 'calendar' || name === 'calendar-event'"
+    :class="props.class"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M4 7a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12z" />
+    <path d="M16 3v4" />
+    <path d="M8 3v4" />
+    <path d="M4 11h16" />
+    <path d="M11 15h1" />
+    <path d="M12 15v3" />
+  </svg>
+
+  <!-- EYE -->
   <svg
     v-else-if="name === 'eye'"
     :class="props.class"
@@ -37,6 +180,7 @@ const props = withDefaults(defineProps<{
     <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
   </svg>
 
+  <!-- SAVE / FLOPPY -->
   <svg
     v-else-if="name === 'save' || name === 'device-floppy'"
     :class="props.class"
@@ -52,6 +196,7 @@ const props = withDefaults(defineProps<{
     <path d="M14 4l0 4l-6 0l0 -4" />
   </svg>
 
+  <!-- DIFF / GIT-COMPARE -->
   <svg
     v-else-if="name === 'diff' || name === 'git-compare'"
     :class="props.class"
@@ -70,6 +215,7 @@ const props = withDefaults(defineProps<{
     <path d="M16 15a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
   </svg>
 
+  <!-- HISTORY -->
   <svg
     v-else-if="name === 'history'"
     :class="props.class"
@@ -84,6 +230,7 @@ const props = withDefaults(defineProps<{
     <path d="M3.05 11a9 9 0 1 1 .5 4m-.5 5v-5h5" />
   </svg>
 
+  <!-- UNDO / REVERT -->
   <svg
     v-else-if="name === 'undo' || name === 'arrow-back-up' || name === 'revert'"
     :class="props.class"
@@ -98,6 +245,7 @@ const props = withDefaults(defineProps<{
     <path d="M5 10h11a4 4 0 1 1 0 8h-1" />
   </svg>
 
+  <!-- COMMAND / PALETTE -->
   <svg
     v-else-if="name === 'command'"
     :class="props.class"
@@ -111,6 +259,7 @@ const props = withDefaults(defineProps<{
     <path d="M7 9a2 2 0 1 1 2 -2v10a2 2 0 1 1 -2 -2h10a2 2 0 1 1 -2 2v-10a2 2 0 1 1 2 2h-10z" />
   </svg>
 
+  <!-- CLOSE / X -->
   <svg
     v-else-if="name === 'close' || name === 'x'"
     :class="props.class"
@@ -125,6 +274,7 @@ const props = withDefaults(defineProps<{
     <path d="M6 6l12 12" />
   </svg>
 
+  <!-- COPY -->
   <svg
     v-else-if="name === 'copy'"
     :class="props.class"
@@ -139,6 +289,7 @@ const props = withDefaults(defineProps<{
     <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" />
   </svg>
 
+  <!-- CHECK -->
   <svg
     v-else-if="name === 'check'"
     :class="props.class"
@@ -152,8 +303,9 @@ const props = withDefaults(defineProps<{
     <path d="M5 12l5 5l10 -10" />
   </svg>
 
+  <!-- SPINNER / LOADER -->
   <svg
-    v-else-if="name === 'spinner' || name === 'loader-2'"
+    v-else-if="name === 'spinner' || name === 'loader-2' || name === 'loading'"
     :class="props.class"
     viewBox="0 0 24 24"
     fill="none"
@@ -165,6 +317,7 @@ const props = withDefaults(defineProps<{
     <path d="M12 3a9 9 0 1 0 9 9" />
   </svg>
 
+  <!-- GRIP -->
   <svg
     v-else-if="name === 'grip' || name === 'grip-vertical'"
     :class="props.class"
@@ -183,6 +336,7 @@ const props = withDefaults(defineProps<{
     <circle cx="15" cy="18" r="1" fill="currentColor" />
   </svg>
 
+  <!-- MINIMIZE / MAXIMIZE -->
   <svg
     v-else-if="name === 'minimize' || name === 'layout-bottombar-collapse'"
     :class="props.class"
@@ -213,6 +367,7 @@ const props = withDefaults(defineProps<{
     <path d="M14 12l-2 -2l-2 2" />
   </svg>
 
+  <!-- SEARCH -->
   <svg
     v-else-if="name === 'search'"
     :class="props.class"
@@ -227,6 +382,7 @@ const props = withDefaults(defineProps<{
     <path d="M21 21l-6 -6" />
   </svg>
 
+  <!-- TRASH -->
   <svg
     v-else-if="name === 'trash'"
     :class="props.class"
@@ -244,6 +400,7 @@ const props = withDefaults(defineProps<{
     <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
   </svg>
 
+  <!-- BOLT / CLOCK -->
   <svg
     v-else-if="name === 'clock-bolt' || name === 'bolt'"
     :class="props.class"
@@ -259,6 +416,7 @@ const props = withDefaults(defineProps<{
     <path d="M19 16l-2 3h4l-2 3" />
   </svg>
 
+  <!-- CROP -->
   <svg
     v-else-if="name === 'crop'"
     :class="props.class"
@@ -277,6 +435,7 @@ const props = withDefaults(defineProps<{
     <path d="M5 16h3" />
   </svg>
 
+  <!-- PHOTO / IMAGE -->
   <svg
     v-else-if="name === 'photo' || name === 'image'"
     :class="props.class"
@@ -293,6 +452,7 @@ const props = withDefaults(defineProps<{
     <path d="M14 14l1 -1a3 5 0 0 1 3 0l2 2" />
   </svg>
 
+  <!-- ADJUSTMENTS / SLIDERS -->
   <svg
     v-else-if="name === 'adjustments' || name === 'sliders'"
     :class="props.class"
@@ -314,6 +474,7 @@ const props = withDefaults(defineProps<{
     <path d="M18 9v11" />
   </svg>
 
+  <!-- UPLOAD -->
   <svg
     v-else-if="name === 'upload' || name === 'cloud-upload'"
     :class="props.class"
@@ -329,8 +490,25 @@ const props = withDefaults(defineProps<{
     <path d="M12 4l0 12" />
   </svg>
 
+  <!-- DOWNLOAD -->
   <svg
-    v-else-if="name === 'rotate' || name === 'rotate-clockwise'"
+    v-else-if="name === 'download'"
+    :class="props.class"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
+    <path d="M7 11l5 5l5 -5" />
+    <path d="M12 4l0 12" />
+  </svg>
+
+  <!-- ROTATE -->
+  <svg
+    v-else-if="name === 'rotate' || name === 'rotate-clockwise' || name === 'refresh'"
     :class="props.class"
     viewBox="0 0 24 24"
     fill="none"
@@ -342,6 +520,7 @@ const props = withDefaults(defineProps<{
     <path d="M4.05 11a8 8 0 1 1 .5 4m-.5 5v-5h5" />
   </svg>
 
+  <!-- FLIP -->
   <svg
     v-else-if="name === 'flip-horizontal'"
     :class="props.class"
@@ -372,6 +551,7 @@ const props = withDefaults(defineProps<{
     <path d="M7 8l10 -4l0 4z" />
   </svg>
 
+  <!-- SPARKLES -->
   <svg
     v-else-if="name === 'sparkles' || name === 'wand'"
     :class="props.class"
@@ -385,6 +565,7 @@ const props = withDefaults(defineProps<{
     <path d="M16 18a2 2 0 0 1 2 2a2 2 0 0 1 2 -2a2 2 0 0 1 -2 -2a2 2 0 0 1 -2 2zm0 -12a2 2 0 0 1 2 2a2 2 0 0 1 2 -2a2 2 0 0 1 -2 -2a2 2 0 0 1 -2 2zm-7 3l-2.5 5.5l-5.5 2.5l5.5 2.5l2.5 5.5l2.5 -5.5l5.5 -2.5l-5.5 -2.5z" />
   </svg>
 
+  <!-- ASPECT-RATIO -->
   <svg
     v-else-if="name === 'aspect-ratio'"
     :class="props.class"
@@ -400,6 +581,24 @@ const props = withDefaults(defineProps<{
     <path d="M17 12v3h-3" />
   </svg>
 
+  <!-- COMPRESS -->
+  <svg
+    v-else-if="name === 'compress'"
+    :class="props.class"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M4 14h6v6" />
+    <path d="M10 14l-7 7" />
+    <path d="M20 10h-6v-6" />
+    <path d="M14 10l7 -7" />
+  </svg>
+
+  <!-- PLAY / PAUSE -->
   <svg
     v-else-if="name === 'pause' || name === 'player-pause'"
     :class="props.class"
@@ -427,6 +626,7 @@ const props = withDefaults(defineProps<{
     <path d="M7 4v16l13 -8z" />
   </svg>
 
+  <!-- FOLDERS -->
   <svg
     v-else-if="name === 'folder'"
     :class="props.class"
@@ -469,6 +669,7 @@ const props = withDefaults(defineProps<{
     <path d="M19 16v6" />
   </svg>
 
+  <!-- CHEVRONS -->
   <svg
     v-else-if="name === 'chevron-down'"
     :class="props.class"
@@ -480,6 +681,19 @@ const props = withDefaults(defineProps<{
     stroke-linejoin="round"
   >
     <path d="M6 9l6 6l6 -6" />
+  </svg>
+
+  <svg
+    v-else-if="name === 'chevron-up'"
+    :class="props.class"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M6 15l6 -6l6 6" />
   </svg>
 
   <svg
@@ -508,6 +722,7 @@ const props = withDefaults(defineProps<{
     <path d="M15 6l-6 6l6 6" />
   </svg>
 
+  <!-- LINK -->
   <svg
     v-else-if="name === 'link'"
     :class="props.class"
@@ -523,6 +738,7 @@ const props = withDefaults(defineProps<{
     <path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" />
   </svg>
 
+  <!-- PLUS -->
   <svg
     v-else-if="name === 'plus'"
     :class="props.class"
@@ -537,6 +753,55 @@ const props = withDefaults(defineProps<{
     <path d="M5 12l14 0" />
   </svg>
 
+  <!-- INFO -->
+  <svg
+    v-else-if="name === 'info'"
+    :class="props.class"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <circle cx="12" cy="12" r="9" />
+    <path d="M12 8h.01" />
+    <path d="M11 12h1v4h1" />
+  </svg>
+
+  <!-- SETTINGS -->
+  <svg
+    v-else-if="name === 'settings' || name === 'gear'"
+    :class="props.class"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066-2.573c-.94-1.543.826-3.31 2.37-2.37c1 .608 2.296.07 2.572-1.065z" />
+    <circle cx="12" cy="12" r="3" />
+  </svg>
+
+  <!-- USERS -->
+  <svg
+    v-else-if="name === 'users' || name === 'user'"
+    :class="props.class"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <circle cx="9" cy="7" r="4" />
+    <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+    <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+    <path d="M21 21v-2a4 4 0 0 0 -3 -3.85" />
+  </svg>
+
+  <!-- FALLBACK DEFAULT (Sparkles / Dot) -->
   <svg
     v-else
     :class="props.class"
@@ -547,6 +812,6 @@ const props = withDefaults(defineProps<{
     stroke-linecap="round"
     stroke-linejoin="round"
   >
-    <circle cx="12" cy="12" r="9" />
+    <path d="M12 3v3m0 12v3m9-9h-3M6 12H3m15.364-6.364l-2.121 2.121M8.757 15.243l-2.121 2.121m12.728 0l-2.121-2.121M8.757 8.757L6.636 6.636" />
   </svg>
 </template>
